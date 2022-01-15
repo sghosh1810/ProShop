05dc0b2481586cfe9e53ac8892a900387b561031
Shounak Ghosh <sghosh1810@gmail.com>
2022-01-15 20:38:01 +0530

    Login frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,10 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 import {Container} from 'react-bootstrap';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 const App = () => {
   return (
     <Router>
@@ -14,6 +16,7 @@ const App = () => {
             <Route path='/' component={HomeScreen} exact />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen}/>
+            <Route path='/login' component={LoginScreen} />
         </Container>
       </main>
       <Footer />
